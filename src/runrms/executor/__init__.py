8b1bc8c6e56487df213b55a87b45be2b5c9a9302
mferrera <matt.ferrera@gmail.com>
2025-11-13 14:04:42 +0100

    ENH: Add ApiExecutor

--- src/runrms/executor/__init__.py
+++ src/runrms/executor/__init__.py
@@ -1,7 +1,9 @@
+from .api_rms_executor import ApiExecutor
 from .fm_rms_executor import FMRMSExecutor
 from .interactive_rms_executor import InteractiveRMSExecutor
 
 __all__ = [
+    "ApiExecutor",
     "FMRMSExecutor",
     "InteractiveRMSExecutor",
 ]
