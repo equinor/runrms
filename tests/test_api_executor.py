6748ae024e15642f7d96b4247b45be1b1696bbc5
mferrera <13275515+mferrera@users.noreply.github.com>
2025-12-08 13:40:11 +0100

    TST: Increase coverage of APIExecutor

--- tests/test_api_executor.py
+++ tests/test_api_executor.py
@@ -1,6 +1,7 @@
 """Tests for the ApiExecutor."""
 
 import subprocess
+from copy import deepcopy
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
@@ -46,6 +47,35 @@ def test_custom_zmq_address_is_used(mock_config: ApiConfig) -> None:
         executor = ApiExecutor(mock_config, zmq_address=custom_address)
 
     assert executor.zmq_address == custom_address
+
+
+def test_setup_rms_environment(mock_config: ApiConfig) -> None:
+    """Correct environment is established for RMS."""
+    # Ensures the env isn't added at initialization
+    with patch.object(ApiExecutor, "_setup_rms_environment"):
+        executor = ApiExecutor(mock_config)
+
+    orig_exec_env = deepcopy(executor._exec_env)
+    executor._setup_rms_environment()
+    new_exec_env = deepcopy(executor._exec_env)
+
+    assert orig_exec_env != new_exec_env
+    assert set(orig_exec_env.items()) ^ set(new_exec_env.items()) == {
+        ("LD_LIBRARY_PATH", "/opt/rms/14.2.2/lib64:/opt/rms/14.2.2/lib"),
+        ("PATH", "/opt/rms/14.2.2/bin"),
+        ("ROXAR_RMS_ROOT", "/opt/rms/14.2.2"),
+    }
+
+
+def test_setup_rms_environment_with_license(mock_config: ApiConfig) -> None:
+    """Correct environment with license file is established for RMS."""
+    with patch.object(ApiExecutor, "_setup_rms_environment"):
+        mock_config.site_config.batch_lm_license_file = "foo.lic"
+        executor_with_license = ApiExecutor(mock_config)
+
+    assert "LM_LICENSE_FILE" not in executor_with_license._exec_env
+    executor_with_license._setup_rms_environment()
+    assert executor_with_license._exec_env["LM_LICENSE_FILE"] == "foo.lic"
 
 
 def test_exec_mode_is_api(mock_config: ApiConfig) -> None:
@@ -98,6 +128,70 @@ def test_build_command(mock_config: ApiConfig, mock_exec_env: dict[str, str]) ->
         assert cmd[-3] == mock_config.wrapper
         assert cmd[-2] == mock_config.executable
         assert cmd[-1] == executor.zmq_address
+
+
+def test_wait_for_starter_startsup_raises_if_no_worker(mock_config: ApiConfig) -> None:
+    """Exception raised if no worker to wait for."""
+    executor = ApiExecutor(mock_config)
+    with pytest.raises(
+        RuntimeError, match="No worker process set, cannot wait for one"
+    ):
+        executor._wait_for_worker_startup()
+
+
+def test_start_worker_does_nothing_if_worker_not_none(mock_config: ApiConfig) -> None:
+    """Start worker is a no-op if the worker process is not none."""
+    executor = ApiExecutor(mock_config)
+    with patch("runrms.executor.api_executor.subprocess.Popen") as mock_popen:
+        executor._worker_process = MagicMock()
+        executor._start_worker()
+        mock_popen.assert_not_called()
+
+
+def test_start_worker_starts_worker_process(mock_config: ApiConfig) -> None:
+    """Start worker creates a worker process and starts it up."""
+    executor = ApiExecutor(mock_config)
+    with patch("runrms.executor.api_executor.subprocess.Popen") as mock_popen:
+        mock_worker_process = MagicMock()
+        mock_worker_process.poll.return_value = None
+        mock_popen.return_value = mock_worker_process
+
+        executor._start_worker()
+
+        mock_popen.assert_called_once_with(
+            executor._build_command(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.DEVNULL,
+        )
+        # Should be called 15+ times
+        mock_worker_process.poll.assert_called()
+        mock_worker_process.communicate.assert_not_called()
+
+
+def test_start_worker_fails_to_start_worker_process(mock_config: ApiConfig) -> None:
+    """Some things are called when a worker process fails to start."""
+    executor = ApiExecutor(mock_config)
+    with (
+        patch("runrms.executor.api_executor.subprocess.Popen") as mock_popen,
+        pytest.raises(RuntimeError, match="stdout: foo\nstderr: bar"),
+    ):
+        mock_worker_process = MagicMock()
+        mock_worker_process.poll.return_value = [None, None, 1]
+        mock_worker_process.communicate.return_value = (b"foo", b"bar")
+        mock_popen.return_value = mock_worker_process
+
+        executor._start_worker()
+
+        mock_popen.assert_called_once_with(
+            executor._build_command(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.DEVNULL,
+        )
+        # Should be called twice (for each None), then fail
+        mock_worker_process.poll.assert_called()
+        mock_worker_process.communicate.assert_called()
 
 
 def test_run_starts_worker_once(mock_config: ApiConfig) -> None:
