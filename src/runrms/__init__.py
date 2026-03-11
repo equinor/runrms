3cdd400886bf6fb82609fd67671277785915f0ee
mferrera <13275515+mferrera@users.noreply.github.com>
2026-03-11 08:57:09 +0100

    FIX: Raise RmsVersionError when getting latest versions

--- src/runrms/__init__.py
+++ src/runrms/__init__.py
@@ -1,5 +1,29 @@
 """runrms - used for running RMS in various ways."""
 
+from runrms.exceptions import (
+    RmsConfigError,
+    RmsConfigNotFoundError,
+    RmsExecutableError,
+    RmsProjectNotFoundError,
+    RmsRuntimeError,
+    RmsVersionError,
+    RmsWrapperError,
+    UnknownConfigError,
+)
+
 from ._rms_api import get_executor, get_rmsapi, shutdown, shutdown_all
 
-__all__ = ["get_rmsapi", "get_executor", "shutdown", "shutdown_all"]
+__all__ = [
+    "get_rmsapi",
+    "get_executor",
+    "shutdown",
+    "shutdown_all",
+    "RmsRuntimeError",
+    "UnknownConfigError",
+    "RmsProjectNotFoundError",
+    "RmsConfigError",
+    "RmsConfigNotFoundError",
+    "RmsExecutableError",
+    "RmsWrapperError",
+    "RmsVersionError",
+]
